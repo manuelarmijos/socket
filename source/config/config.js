bdfd916e002b6b01088f0e8458bc6e3a776c2685
manolo1994 <msarmijiso@gmail.com>
2023-04-16 00:55:21 -0500

    puerto

--- source/config/config.js
+++ source/config/config.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-exports.serverPort = 3005;
+let serverPort = 3005;
 exports.sessionSecret = process.env.SESSION_SECRET;
 exports.rate = {
     windowMs: 5 * 60 * 1000,
