aab3db310889b53da1e6156c9e1d01c96a66c8dc
manolo1994 <msarmijiso@gmail.com>
2023-04-16 22:21:52 -0500

    socket conection

--- source/socket/eventos.js
+++ source/socket/eventos.js
@@ -6,11 +6,13 @@ function enviarEmit(data) {
     config.ejecutarsqlSelect('SELECT socketId FROM  ktaxiSocket.socket where idUsuario = ? ;', [d.idCliente], function (res) {
         if (res.en == 1) {
             console.log('Socket del cliente encontrado')
-            console.log(res.data)
-            console.log(res.data[0])
-            console.log(res.data[0].socketId)
-            socketio.to(res.data[0].socketId).emit("dataConductor", d);
-            console.log('Socket del cliente enviado correctamente')
+            if (res.data.length > 0) {
+                console.log(res.data)
+                console.log(res.data[0])
+                console.log(res.data[0].socketId)
+                socketio.to(res.data[0].socketId).emit("dataConductor", d);
+                console.log('Socket del cliente enviado correctamente')
+            }
         }
         else
             console.log('SQL no EJECUTADO - REGISTRO no GUARDADO')
